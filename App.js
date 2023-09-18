aff9cc34e8ff4b322f4d381cd32771ea03f9e27e
Florian Betin <florian.betin.pro@gmail.com>
2023-09-18 16:16:15 +0200

    q0 a q2

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button, } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.globalcontainer}>
+      <View style={styles.containera}>
+        <Text style={styles.text}>Hello, world!</Text>
+        <Text style={styles.text}>ligne 2</Text>
+        <Text style={styles.text}>ligne 3</Text>
+        <Button
+          title="This is a button"
+        />
+      </View>
+
+      <View style={styles.containerb}>
+        <Text style={styles.text}>Hello, world!</Text>
+        <Text style={styles.text}>ligne 2</Text>
+        <Text style={styles.text}>ligne 3</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  text: {
+    color: "blue",
+  },
+  containera: {
+    backgroundColor: "grey",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  containerb: {
+    backgroundColor: "lightgreen",
+    alignItems: "flex-end",
+  },
+  globalcontainer: {
+    flex: 1,
   },
 });
+
